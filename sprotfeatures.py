459a07accd265e03df5aaa8e7bd77b5e717d9381
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2021-11-16 13:19:28 +0000

    Adding empty file.

---
+++ sprotfeatures.py
@@ -0,0 +1,32 @@
+"""
+Program: sprotfeatures
+File:    sprotfeatures.py
+
+Version:    V1.0
+Date:       16.11.2021
+Function:   Identifies if residue of interest is marked with a relevant 
+            feature in a SwissProt file
+
+
+Author: Barbara A. Mikucka
+
+--------------------------------------------------------------------------
+Description:
+
+
+--------------------------------------------------------------------------
+Usage:
+======
+
+--------------------------------------------------------------------------
+Revision history:
+=================
+V1.0  16.11.21    Original    By: BAM
+"""
+
+#*************************************************************************
+# Import libraries
+
+import sys
+
+#*************************************************************************
