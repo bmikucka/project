9cbf9e4f58da97876faa681aafb281bbcdaf2c49
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2021-11-24 16:04:20 +0000

    Attempted adding feature dictionary for binary result but currently not printing anything.

--- sprotfeatures.py
+++ sprotfeatures.py
@@ -59,9 +59,28 @@ def check_feature (sprot_file):
    #get list of lines from the file
    sprot_lines = read_file(sprot_file)
    #make list of relevant features
-   relevant_fts = ["ACT_SITE", "BINDING", "CA_BIND", "DNA_BIND", "NP_BIND", "METAL", 
-   "MOD_RES", "CARBOHYD", "MOTIF", "LIPID"]
+
+   spfeatures = {
+      "NULL":      (0),
+      'ACT_SITE':  (2**0),
+      'BINDING':   (2**1),
+      'CA_BIND':   (2**2),
+      'DNA_BIND':  (2**3),
+      'NP_BIND':   (2**4),
+      'METAL':     (2**5),
+      'MOD_RES':   (2**6),
+      'CARBOHYD':  (2**7),
+      'MOTIF':     (2**8),
+      'LIPID':     (2**9),
+      'DISULFID':  (2**10),
+      'CROSSLNK':  (2**11)
+   }
+
+   #relevant_fts = ["ACT_SITE", "BINDING", "CA_BIND", "DNA_BIND", "NP_BIND", "METAL", 
+   #"MOD_RES", "CARBOHYD", "MOTIF", "LIPID"]
    #special_fts = ["DISULFID", "CROSSLNK"]
+
+   result = 0
 
    for line in sprot_lines:
       #filter for lines with residue numbers
@@ -88,24 +107,40 @@ def check_feature (sprot_file):
                stop = int(info_list[3])
 
          #check for relevant features
-         if info_list[1] in relevant_fts:
+         
+         if (info_list[1] == 'DISULFID' or info_list[1] == 'CROSSLNK'):
+            if (res_of_interest == start and res_of_interest == stop):
+               print ('bad')
+               print (info_list[1])
+               #add the feature to the result (if only mutated features)
+               result = result | spfeatures[info_list[1]][0]
+            else:
+               print ('ok')
+            #if nonmutated features also need to be added then put the result part here
+         elif info_list[1] in spfeatures:
             #check if residue of interest is in the feature's range
             if (res_of_interest >= start and res_of_interest <= stop):
                #bad for the mutation to be in a relevant feature
                print ('bad')
                print (info_list[1])
+               #add the feature to the result (if only mutated features)
+               result = result | spfeatures[info_list[1]][0]
             else: 
                print ('ok')
-         elif (info_list[1] == 'DISULFID' or info_list[1] == 'CROSSLNK'):
-            if (res_of_interest == start and res_of_interest == stop):
-               print ('bad')
-               print (info_list[1])
-            else:
-               print ('ok')
+            #if nonmutated features also need to be added then put the result part here
+         return result
 
-
+   #create a string to print binary results
+   result = bin(result) 
+   #make result into a string 
+   result = str(result)
+   print (result)
 
 #*************************************************************************
+
+
+
+
 
 
 #take file and residue number from command line
