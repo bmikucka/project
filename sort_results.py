3db77d501f0d423da794dff77366eefea584a8e5
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-02-18 14:09:11 +0000

    Added file for script (empty) for sorting the results data.

---
+++ sort_results.py
@@ -0,0 +1,65 @@
+"""
+Program: sort_results
+File:    sort_results.py
+
+Version:    V1.0
+Date:       18.02.2022
+Function:   Sort JSON files and convert to csv and arff for Weka
+
+
+Author: Barbara A. Mikucka
+
+--------------------------------------------------------------------------
+Description:
+
+
+--------------------------------------------------------------------------
+Usage:
+======
+
+--------------------------------------------------------------------------
+Revision history:
+=================
+V1.0  18.02.22    Original    By: BAM
+"""
+
+#*************************************************************************
+# Import libraries
+
+import sys
+
+
+#*************************************************************************
+
+
+#take a file and copy into subdirectory if does not have errors 
+
+#repeat for each file in the repository
+
+#filter out if no output (no PDB)
+
+#filter out if have sprotFTdist error
+
+
+
+
+#divide all the files in the subdirectory into 10 groups 
+
+#get the SProt_ac from the first file in the 2nd directory 
+#and check if in the last file of the 1st directory
+
+#if yes then move the first file into the 1st directory
+#check again
+
+#repeat for all 10 directories
+
+
+#json2csv for each of the files in the subdirectories
+
+
+#
+
+
+
+
+#
