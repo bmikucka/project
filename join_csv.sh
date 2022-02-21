d3c9c5b062fb74269b156d5aa463008bea8445f0
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-02-21 11:47:46 +0000

    Edited join_csv to use dataset declaration from command line

---
+++ join_csv.sh
@@ -0,0 +1,9 @@
+#join all csvs together - only keep one header
+#take SNP/PD from command line
+
+dataset = $1
+
+# Join the csv files
+   cat ${dataset}csv/* > tmp.csv
+   head -1 tmp.csv > ${dataset}.csv
+   grep -v Binding tmp.csv >> ${dataset}.csv
