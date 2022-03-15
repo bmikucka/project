ac8b829742d8e3f774e6d0ba081c3739d481d7a2
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-03-15 15:00:05 +0000

    Dealing with ?.

--- fix_csv.py
+++ fix_csv.py
@@ -12,75 +12,78 @@ row_count = len(df)
 
 # for column 1 get the value 
 for x in range (row_count):
-distance = float(df._get_value(x, 'SprotFTdist-ACT_SITE'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-ACT_SITE'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+   value = df._get_value(x, 'SprotFTdist-ACT_SITE')
+   if value != '?':
+      distance = float(value)
 
-   distance = float(df._get_value(x, 'SprotFTdist-BINDING'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-BINDING'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-ACT_SITE'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-CA_BIND'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-CA_BIND'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-BINDING'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-BINDING'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-DNA_BIND'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-DNA_BIND'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
-  
-   distance = float(df._get_value(x, 'SprotFTdist-NP_BIND'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-NP_BIND'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-CA_BIND'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-CA_BIND'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-METAL'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-METAL'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-DNA_BIND'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-DNA_BIND'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
+     
+      distance = float(df._get_value(x, 'SprotFTdist-NP_BIND'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-NP_BIND'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-MOD_RES'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-MOD_RES'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-METAL'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-METAL'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-CARBOHYD'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-CARBOHYD'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-MOD_RES'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-MOD_RES'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-MOTIF'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-MOTIF'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-CARBOHYD'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-CARBOHYD'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
-   distance = float(df._get_value(x, 'SprotFTdist-LIPID'))
-   if distance > 15 or distance == -1:
-      #change the value to 15
-      df.loc[x, 'SprotFTdist-LIPID'] = '15'
-      # writing into the file
-      df.to_csv(filename, index=False)
+      distance = float(df._get_value(x, 'SprotFTdist-MOTIF'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-MOTIF'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
+
+      distance = float(df._get_value(x, 'SprotFTdist-LIPID'))
+      if distance > 15 or distance == -1:
+         #change the value to 15
+         df.loc[x, 'SprotFTdist-LIPID'] = '15'
+         # writing into the file
+         df.to_csv(filename, index=False)
 
 
 
