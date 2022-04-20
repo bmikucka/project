fa18efdeb0bc0e979ff8b8c5e3aca248b4f55a52
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-04-20 11:33:28 +0100

    Cleaning up file

--- CreateTrainTest.pl
+++ CreateTrainTest.pl
@@ -1,5 +1,43 @@
 #!/usr/bin/perl
 
+#*************************************************************************
+#
+#   Program:    CreateTrainTest
+#   File:       CreateTrainTest.pl
+#   
+#   Version:    V1.0
+#   Date:       15.03.22
+#   Function:   Create test and training subsets for Weka ML
+#
+#   Copyright:  (c) UCL, Prof. Andrew C. R. Martin, 2022
+#   Author:     Prof. Andrew C. R. Martin
+#   Address:    Institute of Structural and Molecular Biology
+#               Division of Biosciences
+#               University College
+#               Gower Street
+#               London
+#               WC1E 6BT
+#   EMail:      andrew@bioinf.org.uk
+#               
+#*************************************************************************
+#
+#   Description:
+#   ============
+#
+#*************************************************************************
+#
+#   Usage:
+#   ======
+#
+#*************************************************************************
+#
+#   Revision History:
+#   =================
+#   V1.0   15.03.22  Original   By: ACRM
+#
+#*************************************************************************
+
+
 use strict;
 
 my $fold    = shift(@ARGV);
