45675975f99c72458687022eb2797ae97a3b397d
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-04-20 11:18:23 +0100

    Cleaning up file

--- mcc_stats.py
+++ mcc_stats.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
 
+"""
+Program: mcc_stats
+File:    mcc_stats.py
+
+Version:    V1.0
+Date:       16.04.2022
+Function:   Does Shaphiro-Wilk and Mann-Whitney U tests on two datasets.
+
+
+Author: Barbara A. Mikucka
+
+--------------------------------------------------------------------------
+Usage:
+======
+
+--------------------------------------------------------------------------
+Revision history:
+=================
+V1.0  16.04.2022    Original    By: BAM
+"""
+
+#*************************************************************************
+# Import libraries
+
 from scipy import stats
 from scipy.stats import mannwhitneyu
 import sys
@@ -32,7 +56,9 @@ def normal_test (filename):
    """
 
    mcc_results = []    #list of MCC results from dataset 
+   # read file as lines
    lines = read_file (filename)
+   # get mcc values from file lines
    for line in lines:
       infos = line.split()
       mcc_results.append(infos[1])
@@ -65,27 +91,31 @@ def mwu_test (filename_a, filename_b):
    """
 
    mcc_results_a = []    #list of MCC results from dataset A
+   # read file as lines
    lines = read_file (filename_a)
+   # get mcc values from lines for dataset A
    for line in lines:
       infos = line.split()
       mcc_results_a.append(infos[1])
 
    mcc_results_b = []    #list of MCC results from dataset B
+   # read file as lines
    lines = read_file (filename_b)
+   #get mcc values from lines for dataset B
    for line in lines:
       infos = line.split()
       mcc_results_b.append(infos[1])
 
 
 
-   # Getting data in to a dictionary
+   # Getting data in a dictionary format
    data = {'A': mcc_results_a,
             'B': mcc_results_b}
 
    # Dictionary to Dataframe
    df = pd.DataFrame(data)
 
-   #check if difference is significant
+   #check if difference is significant - MWU TEST
    U1, p = stats.mannwhitneyu(df['A'], df['B'])
 
    print("p value is:")
@@ -103,17 +133,18 @@ def mwu_test (filename_a, filename_b):
 
 #*************************************************************************
 
-
-
-
+# files from command line
 file_a = sys.argv[1]
 file_b = sys.argv[2]
 
 print("\n \nDATASET A:")
+# check if dataset A follows normal distribution
 normal_test(file_a)
 
 print("\nDATASET B")
+# check if dataset B follows normal distribution
 normal_test(file_b)
 
 print("\n \nMann-Whitney U Test:")
+# run Mann-Whitney U Test
 mwu_test(file_a, file_b)
