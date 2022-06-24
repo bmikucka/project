82925f23e28f86bb98f9afff91535da5e9b89c69
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2022-06-24 16:09:13 +0100

    fixing mistakes (typos)

--- break_csv.sh
+++ break_csv.sh
@@ -9,8 +9,8 @@ num=$(expr $num - 1) #without the headline
 
 for ((element=0; element<$num; element++))
 do
-	head -1 file > test_0${fold}_${element}.csv
-	line_num=$(expr $element + 2) #starts from 0 and avoid head line
-	sed '${line_num}!d' file >> test_0${fold}_${element}.csv
+	head -1 $file > test_0${fold}_${element}.csv
+	line_num=$(expr $element + 2) #starts from 0 and avoid head line 
+	sed -n ${line_num}p $file >> test_0${fold}_${element}.csv
 
 done
